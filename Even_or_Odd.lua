93e777e0ad4d245c9f11e062bf97f3e667156966
laniakeaGH <111463094+laniakeaGH@users.noreply.github.com>
2022-08-17 23:35:57 +0300

    Create Even_or_Odd.lua

---
+++ Even_or_Odd.lua
@@ -0,0 +1,12 @@
+-- Instructions: Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+local kata = {}
+
+function kata.even_or_odd(number)
+  if number%2 == 0 then -- чётное число при делении на 2 без остатка (0)
+    return 'Even'
+  else -- нечётное число, если при делении на 2 есть остаток (иное число)
+    return 'Odd'
+  end
+end
+
+return kata
