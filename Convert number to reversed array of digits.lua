b0829d27bc2f02e5f547d2c6ce847357cda5b31f
laniakeaGH <111463094+laniakeaGH@users.noreply.github.com>
2022-09-13 23:32:14 +0300

    Create Convert number to reversed array of digits.lua

---
+++ Convert number to reversed array of digits.lua
@@ -0,0 +1,13 @@
+function digitize(n)
+  local reversed_digits = string.reverse(n) -- преобразование числа n в перевёрнутый вид
+  local digits = {} -- изначально пуста
+  
+  for i = 1, #reversed_digits do -- цикл, в котором у меня вставляются значения 
+    -- в таблицу
+    local digit = tonumber(reversed_digits:sub(i, i))
+    table.insert(digits, digit) -- заполняется числами в ходе цикла, функция вставки
+  end
+  
+  return digits -- возвращение итоговой заполненной таблицы
+end
+return digitize
