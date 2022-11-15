b1e5ad0192780fe287f0aeb3952ce9063e911d28
laniakeaGH <111463094+laniakeaGH@users.noreply.github.com>
2022-11-16 00:43:26 +0300

    Update Convert number to reversed array of digits.lua

--- Convert number to reversed array of digits.lua
+++ Convert number to reversed array of digits.lua
@@ -4,7 +4,7 @@ function digitize(n)
   
   for i = 1, #reversed_digits do -- цикл, в котором у меня вставляются значения 
     -- в таблицу
-    local digit = tonumber(reversed_digits:sub(i, i))
+    local digit = tonumber(reversed_digits:sub(i, i)) -- получить цифру из строки на данном индексе
     table.insert(digits, digit) -- заполняется числами в ходе цикла, функция вставки
   end
   
