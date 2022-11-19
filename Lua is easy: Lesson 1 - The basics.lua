3ea1f9e1d5c731d2e425fc4d2c7e763a751f1cd8
laniakeaGH <111463094+laniakeaGH@users.noreply.github.com>
2022-11-19 23:14:11 +0300

    Create Lua is easy: Lesson 1 - The basics.lua

---
+++ Lua is easy: Lesson 1 - The basics.lua
@@ -0,0 +1,11 @@
+kata = {}
+function kata.firstLua(a,b,c)
+ result = a*b
+  if b >= c then
+    return a..' '..result.. ' Lua'
+  else
+    return a..' '..result..' Codewars'
+  end
+end
+
+return kata
